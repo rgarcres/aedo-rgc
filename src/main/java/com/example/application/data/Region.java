2bf7b902c29fb9bf47e8c69f56b45e7d1bb43ce3
Ruben Garcia <rgarcres2211@uma.es>
2025-03-22 12:29:26 +0100

    Entidad Region

--- src/main/java/com/example/application/data/Region.java
+++ src/main/java/com/example/application/data/Region.java
@@ -1,5 +1,20 @@
 package com.example.application.data;
 
-public class Region extends AbstractEntity{
-    
+public class Region extends AbstractEntity {
+    private String nombre;
+    private String provincia;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
 }
