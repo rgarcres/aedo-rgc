1292969ada15e563dcd5a56571063048f4a9add4
Ruben Garcia <rgarcres2211@uma.es>
2025-04-06 12:23:39 +0200

    IniciarSesionView comentado y mejor estructurado, código más limpio

--- src/main/java/com/example/application/views/iniciarsesion/IniciarsesionView.java
+++ src/main/java/com/example/application/views/iniciarsesion/IniciarsesionView.java
@@ -2,10 +2,10 @@ package com.example.application.views.iniciarsesion;
 
 import com.example.application.data.Campanya;
 import com.example.application.data.Grupo;
+import com.example.application.views.utilidades.BotonesCreator;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -28,65 +28,82 @@ import org.vaadin.lineawesome.LineAwesomeIconUrl;
 @Menu(order = 0, icon = LineAwesomeIconUrl.USER)
 public class IniciarsesionView extends Composite<VerticalLayout> {
 
+    //Inicializar las listas que simulan la persistencia en esta demo
     private final List<Campanya> listaCamps = new ArrayList<>();
     private final List<Grupo> listaGrupos = new ArrayList<>();
+    
+    //---------Variables locales---------
+    String nombreUsuario;
 
     public IniciarsesionView() {
-        VerticalLayout layoutColumn2 = new VerticalLayout();
-        H2 h2 = new H2("Iniciar sesión");
-        TextField nombreUsuarioField = new TextField();
+        //---------Crear componentes---------
+        VerticalLayout mainLayout = new VerticalLayout();
+        H2 titulo = new H2("Iniciar sesión");
+        H3 errorMsg = new H3("Introduzca un nombre de usuario para entrar");
+        TextField nombreUsuarioField = new TextField("Nombre");
         Button iniciarSesionButton = new Button("Iniciar sesión");
+
+        //Guardar los atributos en la sesión actual
         VaadinSession.getCurrent().setAttribute("listaCamps", listaCamps);
         VaadinSession.getCurrent().setAttribute("listaGrupos", listaGrupos);
 
+        //---------Configurar Layout---------
+        configurarLayaout(mainLayout);
+        mainLayout.setAlignSelf(FlexComponent.Alignment.CENTER, titulo);
+        titulo.setWidth("max-content");
+        mainLayout.setAlignSelf(FlexComponent.Alignment.CENTER, nombreUsuarioField);
+        nombreUsuarioField.setWidth("min-content");
+        errorMsg.getStyle().set("color", "#ff4e4e");
+        mainLayout.add(titulo);
+
+        //---------Botones---------
+        BotonesCreator.configurarBoton(iniciarSesionButton, "");
+
+        /*
+         * ---------Iniciar Sesion---------
+         * Se inicia sesión si se ha introducido un nombre de usuario
+         * Si no se ha introducido se lanza un mensaje de error 
+         */
+        //Iniciar sesión pulsando el botón
+        iniciarSesionButton.addClickListener(e -> {
+            iniciarSesion(nombreUsuarioField, mainLayout, errorMsg); }
+        );
+        //Iniciar sesión pulsando la tecla Enter
+        nombreUsuarioField.addKeyPressListener(Key.ENTER, e -> {
+            iniciarSesion(nombreUsuarioField, mainLayout, errorMsg);
+        });
+
+        //Añadir componenetes al layout
+        mainLayout.add(nombreUsuarioField);
+        mainLayout.add(iniciarSesionButton);
+    }
+
+    /*
+     * Método que realiza las comprobaciones necesarias para iniciar sesión
+     */
+    private void iniciarSesion(TextField field, VerticalLayout layout, H3 errorMsg){
+        nombreUsuario = field.getValue();
+        layout.remove(errorMsg);
+       
+        if(!nombreUsuario.isEmpty()){
+            VaadinSession.getCurrent().setAttribute("nombreUsuario", nombreUsuario);
+            getUI().ifPresent(ui -> ui.navigate(""));
+        } else {
+            layout.add(errorMsg);
+        }   
+    }
+
+    private void configurarLayaout(VerticalLayout mainLayout){
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         getContent().setJustifyContentMode(JustifyContentMode.CENTER);
         getContent().setAlignItems(Alignment.CENTER);
-        layoutColumn2.setWidthFull();
-        getContent().setFlexGrow(1.0, layoutColumn2);
-        layoutColumn2.setWidth("100%");
-        layoutColumn2.getStyle().set("flex-grow", "1");
-        layoutColumn2.setJustifyContentMode(JustifyContentMode.CENTER);
-        layoutColumn2.setAlignItems(Alignment.CENTER);
-        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, h2);
-        h2.setWidth("max-content");
-        nombreUsuarioField.setLabel("Nombre");
-        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, nombreUsuarioField);
-        nombreUsuarioField.setWidth("min-content");
-        iniciarSesionButton.setWidth("min-content");
-        iniciarSesionButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        iniciarSesionButton.getStyle().set("cursor", "pointer");
-        H3 h3 = new H3("Introduzca un nombre de usuario para entrar");
-        h3.getStyle().set("color", "#ff4e4e");
-
-        getContent().add(layoutColumn2);
-        layoutColumn2.add(h2);
-
-        iniciarSesionButton.addClickListener(
-            e -> {
-                String nombreUsuario = nombreUsuarioField.getValue();
-                layoutColumn2.remove(h3);
-                if(!nombreUsuario.isEmpty()){
-                    VaadinSession.getCurrent().setAttribute("nombreUsuario", nombreUsuario);
-                    getUI().ifPresent(ui -> ui.navigate(""));
-                } else {
-                    layoutColumn2.add(h3);
-                }     
-            }
-        );
-        nombreUsuarioField.addKeyPressListener(Key.ENTER, e -> {
-            String nombreUsuario = nombreUsuarioField.getValue();
-            layoutColumn2.remove(h3);
-            if(!nombreUsuario.isEmpty()){
-                VaadinSession.getCurrent().setAttribute("nombreUsuario", nombreUsuario);
-                getUI().ifPresent(ui -> ui.navigate(""));
-            } else {
-                layoutColumn2.add(h3);
-            }     
-        });
-
-        layoutColumn2.add(nombreUsuarioField);
-        layoutColumn2.add(iniciarSesionButton);
+        mainLayout.setWidthFull();
+        getContent().setFlexGrow(1.0, mainLayout);
+        mainLayout.setWidth("100%");
+        mainLayout.getStyle().set("flex-grow", "1");
+        mainLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        mainLayout.setAlignItems(Alignment.CENTER);
+        getContent().add(mainLayout);
     }
 }
